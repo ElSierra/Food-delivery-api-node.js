1c361f71cd64de0c80bab68b164859e788595cb5
ElSierra <hojoisaac@gmail.com>
2023-05-18 14:56:14 +0100

    fix

--- lib/transporter/transporterInit.ts
+++ lib/transporter/transporterInit.ts
@@ -1,9 +1,20 @@
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.PASSWORD,
+//   },
+// });
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.sendinblue.com",
+  port: 587,
+
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
+    user: process.env.LOGIN_EMAIL,
+    pass: process.env.LOGIN_PASSWORD,
   },
-});
+} as SMTPTransport.Options);
