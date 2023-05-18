ba6af599439203b4800deafd869d450bb2adf6cd
ElSierra <hojoisaac@gmail.com>
2023-05-18 14:43:17 +0100

    revert changes on this file

--- lib/transporter/transporterInit.ts
+++ lib/transporter/transporterInit.ts
@@ -1,20 +1,9 @@
 import nodemailer from "nodemailer";
-import SMTPTransport from "nodemailer/lib/smtp-transport";
-
-// export const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD,
-//   },
-// });
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp-relay.sendinblue.com",
-  port: 587,
-
+  service: "gmail",
   auth: {
-    user: "hojoisaac@gmail.com",
-    pass: "rIZfFXHy06UYmVgQ",
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
-} as SMTPTransport.Options);
+});
